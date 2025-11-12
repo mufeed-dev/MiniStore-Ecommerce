81f1aa473482820d61850b1d67cd9be0d22211b1
mufeed-dev <mufeedpv.dev@gmail.com>
2025-11-12 15:54:39 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Let’s started🚀</h1>
+    </div>
+  );
+};
+
+export default App;
